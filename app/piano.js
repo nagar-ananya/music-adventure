b888e14dd80cab24151b0e8f923773ba146e4707
nagar-ananya <nagar.ananya11@gmail.com>
2023-03-06 21:41:58 -0800

    Piano and home page

--- app/piano.js
+++ app/piano.js
@@ -1,5 +1,7 @@
 import { loadSoundData, getSoundBufferToPlay } from "./sound-config.js";
 
+let context;
+
 function initialize() {
   document.querySelector("#loadButton").addEventListener("click", () => {
     loadSounds();
@@ -10,25 +12,57 @@ function initialize() {
 function loadSounds() {
   loadSoundData().then(() => {
     document.querySelector("#start-message").remove();
+    document.querySelector("#view-container").classList.remove('hide');
     document.body.addEventListener("keydown", (keyEvent) => {
-      playSound(keyEvent.key); 
+      playSound(keyEvent.key);
+      makeKeyPress(keyEvent.key, true);
+    });
+    document.body.addEventListener("keyup", (keyEvent) => {
+      makeKeyPress(keyEvent.key, false);
     });
   });
-
 }
-
 
 // play sound on key click
 function playSound(key) {
   const buffer = getSoundBufferToPlay(key);
   if (buffer) {
-    const context = new AudioContext();
+    console.log(`Playing sound for key ${key}`);
+    if (!context) {
+      context = new AudioContext();
+    }
     const gainNode = context.createGain();
     const source = context.createBufferSource(); // creates a sound source
     source.buffer = buffer; // tell the source which sound to play
-    source.connect(gainNode);
-    gainNode.connect(context.destination);; // connect the source to the context's destination (the speakers)
-    source.start(); // play the source now
+    // source.connect(gainNode);
+    source.connect(context.destination); // connect the source to the context's destination (the speakers)
+    source.start(0); // play the source now
+  }
+}
+
+function makeKeyPress(keyCode, isPressed) {
+  if (!keyCode) {
+    return;
+  }
+  keyCode = keyCode.toLowerCase();
+  const keyDiv = document.querySelector(`#${keyCode}`);
+  const labelDiv = document.querySelector(`#${keyCode}-label`)
+  if (!keyDiv) {
+    return;
+  }
+  if (isPressed) {
+    keyDiv.classList.add("press");
+
+  } else {
+    keyDiv.classList.remove("press");
+  }
+  if (!labelDiv) {
+    return;
+  }
+  if (isPressed) {
+    labelDiv.classList.add("press-text");
+  } else {
+    labelDiv.classList.remove("press-text");
   }
 }
 
--- app/sound-config.js
+++ app/sound-config.js
@@ -1,14 +1,116 @@
 // list of sound files to load
 export const soundConfig = {
+
+
+  // low sounds
+  a: {
+    soundFile: "./sounds/tedagame/f2.ogg",
+    buffer: null,
+  },
+  b: {
+    soundFile: "./sounds/tedagame/g2.ogg",
+    buffer: null,
+  },
+  c: {
+    soundFile: "./sounds/tedagame/a2.ogg",
+    buffer: null,
+  },
+  d: {
+    soundFile: "./sounds/tedagame/b2.ogg",
+    buffer: null,
+  },
+  e: {
+    soundFile: "./sounds/tedagame/c3.ogg",
+    buffer: null,
+  },
+  f: {
+    soundFile: "./sounds/tedagame/d3.ogg",
+    buffer: null,
+  },
+  g: {
+    soundFile: "./sounds/tedagame/e3.ogg",
+    buffer: null,
+  },
+  // medium
+  h: {
+    soundFile: "./sounds/tedagame/a3.ogg",
+    buffer: null,
+  },
+  i: {
+    soundFile: "./sounds/tedagame/b3.ogg",
+    buffer: null,
+  },
+  j: {
+    soundFile: "./sounds/tedagame/c4.ogg",
+    buffer: null,
+  },
+  k: {
+    soundFile: "./sounds/tedagame/d4.ogg",
+    buffer: null,
+  },
+  l: {
+    soundFile: "./sounds/tedagame/e4.ogg",
+    buffer: null,
+  },
+  m: {
+    soundFile: "./sounds/tedagame/f4.ogg",
+    buffer: null,
+  },
+  n: {
+    soundFile: "./sounds/tedagame/g4.ogg",
+    buffer: null,
+  },
+  o: {
+    soundFile: "./sounds/tedagame/a4.ogg",
+    buffer: null,
+  },
   p: {
-    soundFile: "./sounds/tedagame/c5.ogg",
+    soundFile: "./sounds/tedagame/b4.ogg",
     buffer: null,
   },
   q: {
-    soundFile: "./sounds/tedagame/g1.ogg",
+    soundFile: "./sounds/tedagame/c5.ogg",
     buffer: null,
   },
+  r: {
+    soundFile: "./sounds/tedagame/d5.ogg",
+    buffer: null,
+  },
+  s: {
+    soundFile: "./sounds/tedagame/e5.ogg",
+    buffer: null,
+  },
+  t: {
+    soundFile: "./sounds/tedagame/f5.ogg",
+    buffer: null,
+  },
+  u: {
+    soundFile: "./sounds/tedagame/g5.ogg",
+    buffer: null,
+  },
+  v: {
+    soundFile: "./sounds/tedagame/a5.ogg",
+    buffer: null,
+  },
+  w: {
+    soundFile: "./sounds/tedagame/b5.ogg",
+    buffer: null,
+  },
+  x: {
+    soundFile: "./sounds/tedagame/c6.ogg",
+    buffer: null,
+  },
+  y: {
+    soundFile: "./sounds/tedagame/d6.ogg",
+    buffer: null,
+  },
+  z: {
+    soundFile: "./sounds/tedagame/e6.ogg",
+    buffer: null,
+  },
+
 };
+
 
 // function load all the sound files
 export function loadSoundData() {
